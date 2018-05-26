d615ca1f31f8507b7b7d2e7d16c9a75fc3b9c383
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-26 09:40:37 -0300

    Adding more logs to understand the interaction behaviour

--- app/src/main/java/br/com/luisfernandez/recyclerview/std/CustomRecyclerView.java
+++ app/src/main/java/br/com/luisfernandez/recyclerview/std/CustomRecyclerView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -16,6 +17,9 @@ import br.com.luisfernandez.recyclerview.std.adapter.StadiumAdapter;
 
 public class CustomRecyclerView extends RecyclerView
 {
+
+    public static final String TAG = "CustomRecyclerView";
+
     public CustomRecyclerView(Context context)
     {
         super(context);
@@ -37,6 +41,8 @@ public class CustomRecyclerView extends RecyclerView
         View viewSwipedRight = this.findChildViewUnder(e.getX() - this.getWidth(), e.getY());
         if (viewSwipedRight != null && e.getAction() == MotionEvent.ACTION_DOWN) {
             int position = (int) viewSwipedRight.getTag();
+
+            Log.d(TAG, "onTouchPosition: " + position);
 //            StadiumAdapter stadiumAdapter = (StadiumAdapter) getAdapter();
 //            stadiumAdapter.removeAt(position);
 //            stadiumAdapter.notifyDataSetChanged();
@@ -46,5 +52,12 @@ public class CustomRecyclerView extends RecyclerView
         }
 
         return super.onInterceptTouchEvent(e);
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt)
+    {
+
+        super.onScrollChanged(l, t, oldl, oldt);
     }
 }
